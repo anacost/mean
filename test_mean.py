a047b8fac5142d11ea1e24e1e8d86630eaaa1654
Ana Costa Conrado <anacos@schneider.uio.no>
2018-02-09 14:29:44 +0100

    changed test_mean.py

--- test_mean.py
+++ test_mean.py
@@ -24,11 +24,3 @@ def test_long():
     obs = mean(range(1,big))
     exp = big/2.0
     assert obs == exp
-
-def test_complex():
-    # given that complex numbers are an unordered field
-    # the arithmetic mean of complex numbers is meaningless
-    num_list = [2 + 3j, 3 + 4j, -32 - 2j]
-    obs = mean(num_list)
-    exp = NotImplemented
-    assert obs == exp
