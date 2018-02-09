43f28d9399b3ff709ba888335e38f27bb971acb4
Ana Costa Conrado <anacos@schneider.uio.no>
2018-02-09 15:00:42 +0100

    changed mean.py

--- mean.py
+++ mean.py
@@ -1,11 +1,12 @@
 def mean(num_list):
   try:
-        mean = sum(num_list)/len(num_list)
+        mean = sum(num_list)/float(len(num_list))
         if isinstance(mean, complex):
             return NotImplemented
         return mean
-  except ZeroDivisionError :
-        return 0
+  except ZeroDivisionError as detail:
+        msg="\nCannot compute the mean value of an empty list"
+        raise ZeroDivisionError(detail.__str__() + "\n" +  msg)
   except TypeError as detail :
         msg = "The algebraic mean of an non-numerical list is undefined.\
                Please provide a list of numbers."
